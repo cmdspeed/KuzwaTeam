bd51f4c3f25cd7ef9848b7bdae8cd7e4a68882e5
cmdspeed <cmdspeed@gmail.com>
2024-03-02 15:12:30 +0100

    add component JoinUs

--- src/components/Footer/Footer.tsx
+++ src/components/Footer/Footer.tsx
@@ -1,55 +1,14 @@
 import { Logo } from "../Logo/Logo";
-import icons from "../../assets/icon/icon.svg";
+
 import { Address } from "./Address/Address";
+import { JoinUs } from "./JoinUs/JoinUs";
 
 export const Footer = () => {
   return (
     <footer>
       <Logo />
       <Address />
-      <div>
-        <h2>Join Us</h2>
-        <div>
-          <div>
-            <a
-              href="http://intagram.com"
-              target="_blank"
-              rel="noopener noreferrer"
-            >
-              <svg width="20px" height="20px">
-                <use href={`${icons}#instagram`} />
-              </svg>
-            </a>
-            <a
-              href="http://twitter.com"
-              target="_blank"
-              rel="noopener noreferrer"
-            >
-              <svg width="20px" height="20px">
-                <use href={`${icons}#twitter`} />
-              </svg>
-            </a>
-            <a
-              href="http://facebook.com"
-              target="_blank"
-              rel="noopener noreferrer"
-            >
-              <svg width="20px" height="20px">
-                <use href={`${icons}#facebook`} />
-              </svg>
-            </a>
-            <a
-              href="http://linkedin.com"
-              target="_blank"
-              rel="noopener noreferrer"
-            >
-              <svg width="20px" height="20px">
-                <use href={`${icons}#linkedin`} />
-              </svg>
-            </a>
-          </div>
-        </div>
-      </div>
+      <JoinUs />
     </footer>
   );
 };
